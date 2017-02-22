e64a38be7453929ed70aa466fa13b499e490c47f
SamWij <sam.wijesinha@gmail.com>
2017-02-22 14:16:13 +1100

    functions complete, winning combination to be finalised

--- javascript/app.js
+++ javascript/app.js
@@ -1,31 +1,84 @@
 console.log("tic tac toe connected");
 
-var player1Array = [];
-var winningCombinations = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+var playerScore = [];
+var computerScore = [];
 
+var board = [1,2,3,4,5,6,7,8,9];
+var playerTurn = 0;
+var ties = 0;
+var player = "X";
+var computer = "O";
+var winner = "none"
 
-var player1 = $( "#X" ).click(function() {
-  alert( "Handler X called." );
-   var player = "X";
+var playerClick =$(".boardPiece").click(function() {
+
+  var playerPick = $(this).attr('id');
+
+  playGame(playerPick, board);
 
 });
 
-var player2 = $( "#O" ).click(function() {
-  alert( "Handler O called." );
-  var player = "O";
+var updateBoard = function(playerPick, board) {
 
-});
+    var itemtoRemove = parseInt(playerPick);
+
+    board.splice($.inArray(itemtoRemove, board),1);
+
+}
+
+ var playGame = function(playerPick, board) {
+
+   if ($("#"+playerPick).children().length === 0){
+        $("#"+playerPick).append("<img src='images/player1.png' />");
+
+        playerScore.push(parseInt(playerPick));
+
+        updateBoard(playerPick, board)
+
+        computerMove(playerPick, board);
+
+        checkforWinner(playerPick, playerScore, computerScore, board);
+   }
 
 
-$( ".boardPiece" ).click(function() {
-
-  var player1Pick = $(this).attr('id');
-  $("#"+player1Pick).append("<img src='images/player1.png' />");
-  player1Array.push(player1Pick);
-
-  computer();
-
-  });
+   }
 
 
-var computer = 
+ function computerMove(playerPick, board) {
+
+    var box = board[Math.floor(Math.random()* board.length)]
+
+     if($("#"+box).children('img').length !== 1) {
+
+       $("#"+box).append("<img src='images/player2.png' />");
+       computerScore.push(box)
+       updateBoard(box, board)
+
+     } else  {
+      checkforWinner();
+     }
+};
+
+function checkforWinner(playerPick, playerScore, computerScore, board) {
+
+  var winningCombinations = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [7,5,3], [1,5,9]]
+
+for (var i = 0; i < winningCombinations.length; i++) {
+  for (var j = 0; j < winningCombinations[i].length; j++) {
+    if (winningCombinations[i][j] === playerPick) {
+      var winPoint =  winningCombinations[i][j]
+
+      console.log(winPoint + " " + playerPick)
+    }
+
+
+  }
+
+}
+    };
+
+
+function clearBoard() {
+
+  $( ".boardPiece" ).empty();
+}
